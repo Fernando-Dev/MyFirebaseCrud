e2866206caf934dcbd67f2cc5e2bee471ac99ca7
Fernando Nascimento <fernando_dev@outlook.com.br>
2023-07-01 18:46:05 -0300

    criando o adapter da lista

---
+++ app/src/main/java/com/fntechma/myfirebasecrud/ui/MainAdapter.kt
@@ -0,0 +1,31 @@
+package com.fntechma.myfirebasecrud.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter: RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind() {
+
+        }
+
+
+    }
+
+}
